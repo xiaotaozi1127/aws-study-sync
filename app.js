f218a5e332fee9c4784405808a25d044fb382085
EC2 Default User <ec2-user@ip-172-31-13-166.ec2.internal>
2020-09-23 05:38:08 +0000

    [taohui] change title

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var data = {
-  title: 'Hello World!',
+  title: 'Study-Sync V2',
   selected: null,
   question: 'In the Battle of Wolf 359 what prominent Miranda-class ship was destroyed?',
   choices: [
